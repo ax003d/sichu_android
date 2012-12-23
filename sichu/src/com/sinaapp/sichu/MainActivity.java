df9565a611eabcc0d114e0a15b4e8a1e404cd77d
ax003d <ax003d@gmail.com>
2012-12-23 18:01:45 +0800

    * extends from sliding activity

--- sichu/src/com/sinaapp/sichu/MainActivity.java
+++ sichu/src/com/sinaapp/sichu/MainActivity.java
@@ -1,17 +1,27 @@
 package com.sinaapp.sichu;
 
-import org.holoeverywhere.app.Activity;
-
-import com.actionbarsherlock.view.Menu;
+import org.holoeverywhere.slidingmenu.SlidingActivity;
+import org.holoeverywhere.slidingmenu.SlidingMenu;
 
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.view.Menu;
+import com.sinaapp.sichu.widget.NavigationWidget;
+
+public class MainActivity extends SlidingActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+		
+		NavigationWidget navigationWidget = new NavigationWidget(this);
+		setBehindContentView(navigationWidget);
+		
+        final SlidingMenu si = getSlidingMenu();
+        si.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        si.setBehindWidthRes(R.dimen.demo_menu_width);
+        si.setShadowWidth(0);
 	}
 
 	@Override
